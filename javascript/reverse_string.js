d51fa9bac9d5424530447ec9aabf931789c2d802
Anri <anri0806@hotmail.co.jp>
2022-12-29 16:16:57 -0500

    my incompleted answer

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
   // type your code here
+  let splitted = str.split("");
+  let i = [];
+  for (let i = 0; i < str.length; i++) {
+    // console.log(splitted[i]);
+    return splitted[i];
+  }
 }
 
 if (require.main === module) {
@@ -16,4 +22,9 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+///// I want to print string from the last length to first length
+///// First, separate string
+///// Then, iterate backward
+
 // And a written explanation of your solution
