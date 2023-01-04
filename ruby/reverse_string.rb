0244659b673f0d4ff58f72866c0308840304093c
Anri <anri0806@hotmail.co.jp>
2023-01-03 23:02:33 -0500

    my answer

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,41 @@
 def reverse_string(str)
   # type your code in here
+
+  ### Using while method
+
+  # Varilable to store iterated letter
+  newStr = ""
+
+  # While i is less than string length (5), in other words,
+  # while i is less than 5, add iterated letter in front of exsiting letter
+  # increment i
+  i = 0
+  while i < str.length
+    newStr = str[i] + newStr
+    i += 1
+  end
+
+  return newStr
+
+  
+
+  ### Using .each method
+
+  ## Split str and create empty variable to store letters
+  # newStr = str.split("")
+  # reversedStr = []
+
+  ## Iterate each letter and add in front of previous letter
+  # newStr.each do |s| 
+  #   reversedStr.unshift(s)
+  # end
+
+  # return reversedStr.join("")
+
+  ### Using .reverse method
+  # newStr = str.split("").reverse().join("")
+  # return newStr
+  
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,4 +51,9 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+## I want to input reveresed string
+
+## First, create variable and split string 
+## Then, iterate each letter AND ADD LETTER IN FRONT OF PREVIOUS LETTER
+
 # And a written explanation of your solution
