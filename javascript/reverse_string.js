437728d5279e3cdc6eb5dbe5ad19dd69e6b9a9a2
Anri <anri0806@hotmail.co.jp>
2023-01-31 23:11:00 -0500

    second attempt

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,17 +1,13 @@
 function reverseString(str) {
   // type your code here
 
-  let newString = "";
+  let newStr = [];
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    newString += str[i];
+  for (let i = str.length-1; i >= 0; i--) {
+    newStr += str[i];
   }
 
-  return newString;
-
-  // let newStr = str.split("").reverse().join("");
-
-  // return newStr
+  return newStr;
 }
 
 if (require.main === module) {
@@ -28,10 +24,32 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+//// Second Time /////
+// I want to console log reversed word using iteration
+// First, create empty variable to store iterated letter
+// Second, iterate word from last length then add to above variable
+// Then, return variable
+
+//// FIrst Time /////
 //// I want to input reversed letters 1 by 1
 //// First, create empty variable to store iterated letters
 //// Iterate from last length-1, until equal to 0, decrement
 //// Add iterated letters to variable
 //// Then, return variable
 
-// And a written explanation of your solution
+// function reverseString(str) {
+//   // type your code here
+
+//   let newString = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+
+//   return newString;
+
+//   // let newStr = str.split("").reverse().join("");
+
+//   // return newStr
+// }
